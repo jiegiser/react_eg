68d800530de613f943e12e2244096933959a6391
jiegiser <cjieouba@outlook.com>
2020-02-06 10:07:48 +0800

    Init: 初始化项目

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+/*
+ * @Descripttion: 
+ * @Author: jiegiser
+ * @Date: 2020-02-04 11:58:19
+ * @LastEditors  : jiegiser
+ * @LastEditTime : 2020-02-04 13:06:49
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// pwa技术
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
